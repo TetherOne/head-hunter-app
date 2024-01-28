068cb428ee6a4af75b8492800a77173b63fe07bd
Pavel Ryaguzov <pashajobber@gmail.com>
2024-01-29 00:05:27 +0500

    ad postgre

--- core/config.py
+++ core/config.py
@@ -4,14 +4,17 @@ from pydantic import BaseModel
 
 from pathlib import Path
 
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 
-BASE_DIR = Path(__file__).parent.parent
-
-DB_PATH = BASE_DIR / 'db.sqlite3'
+# BASE_DIR = Path(__file__).parent.parent
+#
+# DB_PATH = BASE_DIR / 'db.sqlite3'
 
 class DbSettings(BaseModel):
-    url: str = f'sqlite+aiosqlite:///{DB_PATH}'
+    # url: str = f'sqlite+aiosqlite:///{DB_PATH}'
+
+    url: str = f'postgresql+asyncpg://postgres:qwerty@localhost:5432/head_hunter'
     echo: bool = False
 
 
@@ -23,3 +26,6 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+engine = create_async_engine(settings.db.url)
+async_session_maker = async_sessionmaker(engine)
---
+++ tests/test_resume.py
@@ -0,0 +1,3 @@
+def test_register():
+
+    assert 1 == 2
