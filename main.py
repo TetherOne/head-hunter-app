802b7ef8f2589f78563a9e2f9203bbce8185a129
Pavel Ryaguzov <pashajobber@gmail.com>
2024-01-28 22:33:30 +0500

    ad templates

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from contextlib import asynccontextmanager
 from api_v1 import main_router as router_v1
 
 from core.config import settings
-
+from fastapi import Request
 from fastapi import FastAPI
-
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 
@@ -21,13 +21,13 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
+templates = Jinja2Templates(directory='templates')
 
-  
+
+
 @app.get('/')
-def hello_world():
-    return {
-        'message': 'hellow world'
-    }
+def hello_world(request: Request):
+    return templates.TemplateResponse('main_page.html', {'request': request})
 
 
 
