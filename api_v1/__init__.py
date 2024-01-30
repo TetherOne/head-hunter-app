27f5d52820ff3472716a60101d1664e12d6717db
Pavel Ryaguzov <pashajobber@gmail.com>
2024-01-30 20:34:22 +0500

    replace auth routers from main.pu to __ini__.py in api_v1

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,11 +1,19 @@
-from api_v1.auth.auth import auth_backend
 from api_v1.auth.manager import get_user_manager
-from api_v1.auth.schemas import UserRead, UserCreate
+
 from api_v1.resume.views import resume_router
+
+from api_v1.auth.schemas import UserCreate
+from api_v1.auth.schemas import UserRead
 
 from api_v1.users.views import user_router
 
+from api_v1.auth.auth import auth_backend
+
+from fastapi_users import FastAPIUsers
+
 from fastapi import APIRouter
+
+from core.models import User
 
 
 
@@ -15,3 +23,26 @@ main_router = APIRouter()
 
 main_router.include_router(router=resume_router, prefix='/resume')
 main_router.include_router(router=user_router, prefix='/users')
+
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+
+
+main_router.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+
+
+main_router.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-from api_v1.auth.manager import get_user_manager
-
-from fastapi.templating import Jinja2Templates
-
 from api_v1 import main_router as router_v1
 
 from contextlib import asynccontextmanager
-
-from api_v1.auth.auth import auth_backend
-
-from api_v1.auth.schemas import UserCreate
-from api_v1.auth.schemas import UserRead
-
-from fastapi_users import FastAPIUsers
 
 from core.config import settings
 
@@ -19,8 +8,6 @@ from fastapi import Request
 from fastapi import FastAPI
 
 import uvicorn
-
-from core.models import User
 
 
 
@@ -32,33 +19,6 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
-
-
-
-fastapi_users = FastAPIUsers[User, int](
-    get_user_manager,
-    [auth_backend],
-)
-
-
-
-app.include_router(
-    fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
-    tags=["auth"],
-)
-
-
-
-app.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
-    tags=["auth"],
-)
-
-
-
-templates = Jinja2Templates(directory='templates')
 
 
 
