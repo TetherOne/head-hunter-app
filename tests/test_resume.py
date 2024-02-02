ccedaaaa077fcc0e6d0d477e5510a85afea1fab7
Pavel Ryaguzov <pashajobber@gmail.com>
2024-02-02 22:45:46 +0500

    need update test_delete_resume

--- tests/test_resume.py
+++ tests/test_resume.py
@@ -8,49 +8,49 @@ client = TestClient(app)
 
 
 
-# def test_create_resume():
-#     response = client.post(
-#         "http://127.0.0.1:8000/api/v1/resume",
-#         json={
-#           "user_id": 29,
-#           "job_name": 'водила',
-#           "skills": 'быстро еду',
-#           "experience": "10 лет",
-#           "salary": 100000
-#         }
-#     )
-#
-#     if response.status_code == 201:
-#
-#         assert True
-#
-#     else:
-#
-#         print('Неправильная валидация данных при создании резюме.')
-#         assert False
+def test_create_resume():
+    response = client.post(
+        "http://127.0.0.1:8000/api/v1/resume",
+        json={
+          "user_id": 29,
+          "job_name": 'водила',
+          "skills": 'быстро еду',
+          "experience": "10 лет",
+          "salary": 100000
+        }
+    )
+
+    if response.status_code == 201:
+
+        assert True
+
+    else:
+
+        print('Неправильная валидация данных при создании резюме.')
+        assert False
 
 
-# def test_update_resume():
-#     resume_id = 25
-#     response = client.put(
-#         f'http://127.0.0.1:8000/api/v1/resume/{resume_id}',
-#         json={
-#             "user_id": 24,
-#             "job_name": 'шахтер вася пупкин',
-#             "skills": 'быстро копаю',
-#             "experience": "10 лет",
-#             "salary": 10000,
-#         }
-#     )
-#
-#     if response.status_code == 200:
-#
-#         assert True
-#
-#     else:
-#
-#         print('Неправильная валидация данных при обновлении резюме.')
-#         assert False
+def test_update_resume():
+    resume_id = 25
+    response = client.put(
+        f'http://127.0.0.1:8000/api/v1/resume/{resume_id}',
+        json={
+            "user_id": 24,
+            "job_name": 'шахтер вася пупкин',
+            "skills": 'быстро копаю',
+            "experience": "10 лет",
+            "salary": 10000,
+        }
+    )
+
+    if response.status_code == 200:
+
+        assert True
+
+    else:
+
+        print('Неправильная валидация данных при обновлении резюме.')
+        assert False
 
 
 
@@ -71,3 +71,20 @@ def test_update_resume_partial():
 
         print('Неправильная валидация данных при обновлении резюме.')
         assert False
+
+
+
+# def test_delete_resume():
+#     resume_id = 15
+#     response = client.delete(
+#         f'http://127.0.0.1:8000/api/v1/resume/{resume_id}'
+#     )
+#     print(response.json())
+#     if response.status_code == 204:
+#
+#         assert True
+#
+#     else:
+#
+#         print('Ошибка при удалении резюме, проверьте resume_id')
+#         assert False
