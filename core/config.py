382a0d5a9cbe49316196889061952dd9a5e0b27f
Pavel Ryaguzov <pashajobber@gmail.com>
2024-02-08 12:30:25 +0500

    update password

--- core/config.py
+++ core/config.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 
 class DbSettings(BaseModel):
 
-    url: str = f'postgresql+asyncpg://postgres:qwerty@db_hh:5432/head_hunter'
+    url: str = f'postgresql+asyncpg://head_hunter_test:Wrb27C2l13aG8c1un6gbXwtKyqa8sISa@dpg-cn281sol6cac739b7e40-a:5432/head_hunter'
     echo: bool = False
 
 
