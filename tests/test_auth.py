1c2d55163d85c776b73ed8ee37de545ae4b0c494
Pasha Ryaguzov <140720528+TetherOne@users.noreply.github.com>
2024-04-30 21:36:33 +0500

    Update test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,9 +3,7 @@ from fastapi.testclient import TestClient
 from main import app
 
 
-
 client = TestClient(app)
-
 
 
 def test_register():
@@ -20,17 +18,12 @@ def test_register():
             "username": "test_user20"
         }
     )
-
+    
     if response.status_code == 201:
-
         print("Пользователь успешно зарегистрирован")
         assert True
-
     elif response.status_code == 400:
-
         print('Пользователь с такими данными уже существует.')
         assert False
-
     else:
-
         assert False
